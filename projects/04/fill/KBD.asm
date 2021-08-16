982f62a1ae20a3ebfc45271c74a05ada769f4109
ee7klt <grep1984@pm.me>
2021-08-16 18:26:27 +0800

    working fill in KBD.asm

--- projects/04/fill/KBD.asm
+++ projects/04/fill/KBD.asm
@@ -1,37 +1,41 @@
-
-
 @SCREEN
 D = A
 @address
 M = D			// address = 16384 (base address of the Hack screen)
 
-@i
-M = 0			// i = 0. need to do this every time the key
-                    // status is changed. put inside loop
-
-
 (LISTENING)
 @KBD
 D = M
+
 @PRESSED
 D;JGT     // if key is depressed, scan code appears
-@LISTENING
-0;JMP
 
-// fill
-(PRESSED)
+@UNPRESSED
+D;JMP
 
-   @i
-   D = M        // D = i
-
+(UNPRESSED)
+   @0
+   D=A          // D = 8191
    @address
-   A = D+M		// A = @SCREEN + i
-   M = -1		// RAM[A] = -1 (16 pixels)
- 
-   @i
-   M = M + 1	// i = i + 1
-
+   A=M         // A = 16354 (M[address])
+   M = D		// RAM[16354] = M[@SCREEN] = + 2^15-1 (16 pixels)
    @LISTENING
    0;JMP
+
+(PRESSED)
+   @address
+   A=M         // A = 16354
+   M = -1		// RAM[A] = -1 (16 pixels)
+   @LISTENING
+   0;JMP
+
+
+
+
+
+
+
+
+
 
 
